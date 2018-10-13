fd9c68bcec98c7a7b694f1048aaaeeea3d56a01d
nic <nicster34@gmail.com>
2018-10-13 15:51:08 -0400

    zip aded

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,13 +17,24 @@ public class DemoApplication {
     return "New Music Coming Soon!";
   }*/
   @GetMapping("/WeatherData")
-  public String getWeatherData(){
-      Sounder sounder = new Sounder();
+  public String getWeatherData(@RequestParam("zip") String zipcode){
+          Sounder  sounder = new Sounder();
 	  RestTemplate restTemplate = new RestTemplate();
-	  Weather weather = restTemplate.getForObject("http://api.openweathermap.org/data/2.5/forecast?zip=48340&APPID=cf32c0bcc723a965df94aeb04388c1ee", Weather.class);
+	  Weather weather;
+	  if(zipcode.isEmpty()){
+	  weather = restTemplate.getForObject("http://api.openweathermap.org/data/2.5/forecast?zip=48340&APPID=cf32c0bcc723a965df94aeb04388c1ee", Weather.class);}
+	  else{
+	  weather = restTemplate.getForObject("http://api.openweathermap.org/data/2.5/forecast?zip="+zipcode+"&APPID=cf32c0bcc723a965df94aeb04388c1ee", Weather.class);
+	  }
 	  double temp = weather.getWeatherList().get(0).getMain().getTemp();
 	  double windSpeed = weather.getWeatherList().get(0).getWind().getSpeed();
 	  System.out.println(sounder.getMusicLink(sounder.getMusicSettings(weather.getWeatherList().get(0))));
     return sounder.getMusicLink(sounder.getMusicSettings(weather.getWeatherList().get(0)));
+  }
+  @GetMapping("/WeatherJson")
+  public Weather getWeatherInfo(){
+	RestTemplate restTemplate = new RestTemplate();
+	Weather weather = restTemplate.getForObject("http://api.openweathermap.org/data/2.5/forecast?zip=48340&APPID=cf32c0bcc723a965df94aeb04388c1ee", Weather.class);
+	return weather;
   }
 }
