d538451372eda583e4aab62581e201a096d67e8f
Airsick <corauh@umflint.edu>
2018-10-13 17:06:02 -0400

    poosh

--- src/main/java/com/example/demo/Sounder.java
+++ src/main/java/com/example/demo/Sounder.java
@@ -14,53 +14,6 @@ public class Sounder {
 
 	}
 
-	/*public List<Weather> getWeatherDatas(String json) {
-		ObjectMapper mapper = new ObjectMapper();
-		List<Weather> weathers = new ArrayList<Weather>();
-		try {
-			JsonNode nodes = mapper.readTree(json);
-			int i = 0;
-			while(nodes.path("list").has(i)) {
-				Weather weather = new Weather();
-				weather.clouds = nodes.path("list").path(i).path("clouds").path("all").intValue();
-				weather.temp = nodes.path("list").path(i).path("main").path("temp").doubleValue();
-				weather.wind = nodes.path("list").path(i).path("wind").path("speed").doubleValue();
-				weather.precipitation = nodes.path("list").path(i).path("rain").path("3h").doubleValue() +
-						nodes.path("list").path(i).path("snow").path("3h").doubleValue();
-				weather.weather = nodes.path("list").path(i).path("weather").path(0).path("description").textValue();
-				weathers.add(weather);
-				i++;
-			}
-		}catch (JsonParseException e){
-			System.out.print(("see"));
-		}catch (JsonMappingException e){
-			System.out.print("broke");
-		}catch (IOException e){
-			System.out.print("nothin");
-		}
-		return weathers;
-	}
-
-	public Weather getWeatherData(String json) {
-		ObjectMapper mapper = new ObjectMapper();
-		Weather weather = new Weather();
-		try {
-			JsonNode nodes = mapper.readTree(json);
-			weather.clouds = nodes.path("list").path(0).path("clouds").path("all").intValue();
-			weather.temp = nodes.path("list").path(0).path("main").path("temp").doubleValue();
-			weather.wind = nodes.path("list").path(0).path("wind").path("speed").doubleValue();
-			weather.precipitation = nodes.path("list").path(0).path("rain").path("3h").doubleValue()+
-					nodes.path("list").path(0).path("snow").path("3h").doubleValue();
-			weather.weather = nodes.path("list").path(0).path("weather").path(0).path("description").textValue();
-		}catch (JsonParseException e){
-			System.out.print(("see"));
-		}catch (JsonMappingException e){
-			System.out.print("broke");
-		}catch (IOException e){
-			System.out.print("nothin");
-		}
-		return weather;
-	}*/
 
 	public MusicSettings getMusicSettings(WeatherList weather) {
 		MusicSettings settings = new MusicSettings();
@@ -68,7 +21,9 @@ public class Sounder {
 		// temperature -> tempo
 		settings.tempo = (int)((weather.getMain().getTemp() -244)/(333-244)*130+50);
 		//
-		settings.pitch = (int)((1 - (weather.getRain().getThreeHours() - 0) / 1)*48+24);
+		if(weather.getRain()!=null) {
+			settings.pitch = (int) ((10.0 - weather.getRain().getThreeHours()) / 10.0 * 48.0 + 24.0) < 24 ? 24 : (int) ((10.0 - weather.getRain().getThreeHours()) / 10.0 * 48.0 + 24.0);
+		}
 		settings.duration = (int)(settings.tempo*2.5);
 
 		settings.instruments[0]=1;
