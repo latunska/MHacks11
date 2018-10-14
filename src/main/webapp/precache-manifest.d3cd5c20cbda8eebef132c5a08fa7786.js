4a50fa635e2a7738fcb2f4aab6ab7d3cde176c47
nic <nicster34@gmail.com>
2018-10-14 00:43:12 -0400

    fixed

---
+++ src/main/webapp/precache-manifest.d3cd5c20cbda8eebef132c5a08fa7786.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "419b904305f6a69274aff9561a66d5be",
+    "url": "/static/media/snow.419b9043.svg"
+  },
+  {
+    "revision": "713441ebaa52aee7105a34860b55f84e",
+    "url": "/static/media/Ring.713441eb.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "124ec6a507a480b63fe2",
+    "url": "/static/js/main.124ec6a5.chunk.js"
+  },
+  {
+    "revision": "510b5ee507aef99d6540",
+    "url": "/static/js/1.510b5ee5.chunk.js"
+  },
+  {
+    "revision": "124ec6a507a480b63fe2",
+    "url": "/static/css/main.99b82ff4.chunk.css"
+  },
+  {
+    "revision": "514c26f9317a09d6b640ad6c74397497",
+    "url": "/index.html"
+  }
+];
